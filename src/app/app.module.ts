ec5949141fea8ab377140825f9205968fc455ccd
Abdelfattah Ragab <mscaragab@gmail.com>
2021-01-15 01:23:26 +0200

    'Update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AppComponent,
     HeaderComponent,
     SidenavComponent,
-    NotFoundComponent
   ],
   imports: [
     BrowserModule,
